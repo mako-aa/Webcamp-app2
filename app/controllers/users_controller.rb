9a4297b206eaa18a23ed6972fa8e9ab6fda4e840
mako-aa <mako00922@gmail.com>
2024-09-19 11:35:52 +0000

    [Finish]4-last

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  
+  before_action :is_matching_login_user, only: [:edit, :update]
+  
   def show
     @user = User.find(params[:id])
     #22 1ページ分の決められた数のデータだけを新しい順に取得する
@@ -11,6 +14,7 @@ class UsersController < ApplicationController
   end
   
   def update
+    
     @user = User.find(params[:id])
     @user.update(user_params)
     redirect_to user_path(@user.id)
@@ -21,5 +25,13 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :profile_image)
   end
+  
+  def is_matching_login_user
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to post_images_path
+    end 
+  end 
+  
   
 end
